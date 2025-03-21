5aca97fca6ec19ca6fb7e7a2dd6368920dc0d4da
D14m0nDS <80215134+D14m0nDS@users.noreply.github.com>
2025-03-21 19:11:08 +0200

    added dependencie property in the subtask model

--- server/app/model/subtask_model.py
+++ server/app/model/subtask_model.py
@@ -1,12 +1,14 @@
 from app.extensions import db
+from sqlalchemy.dialects.postgresql import ARRAY
 
 class Subtask(db.Model):
     __tablename__ = "subtask"
 
     id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     title = db.Column(db.String(100), nullable=False)
+    dependency_ids = db.Column(ARRAY(db.Integer), nullable=False, default=[])  # Array of dependency IDs
 
     project = db.relationship('Project', backref='subtasks')
 
